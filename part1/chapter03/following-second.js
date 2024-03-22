0fef8a3a72405b47adcb2812a31e78fedeb8d94d
mohamed <khaldimohamed82@gmail.com>
2024-03-22 23:22:12 +0000

    Notes from my learning journey

--- part1/chapter03/following-second.js
+++ part1/chapter03/following-second.js
@@ -1,12 +1,26 @@
+/*
+Following second
+Write a program that asks for a time under the form of three information (hours, minutes, seconds). The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
+
+This is not as simple as it seems... Look at the following results to see for yourself:
+
+14h17m59s should give 14h18m0s
+6h59m59s should give 7h0m0s
+23h59m59s should give 0h0m0s (midnight)
+
+*/
+
+// The solution
+
 const prompt = require('prompt-sync')();
 
 const hours = Number(prompt('Enter the hours: '));
 const minutes = Number(prompt('Enter the minutes: '));
 const seconds = Number(prompt('Enter the seconds: '));
+
 if(!Number(hours) || !Number(minutes) || !Number(seconds)){
   console.log("Pleas enter a valid time");
 }
-
 else if(hours == 23 && minutes == 59 && seconds == 59){
   console.log('00h:00m:00s');
 }
@@ -19,3 +33,8 @@ else if (seconds == 59){
 else{
   console.log(`${hours}h:${minutes}m:${seconds +1}s`)
 }
+
+
+
+
+
