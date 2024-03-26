54f886c89cc61558751cb087eba7854363dc2ee1
mohamed <khaldimohamed82@gmail.com>
2024-03-26 23:19:14 +0000

    Notes from my learning journey

--- part1/chapter04/input-validation.js
+++ part1/chapter04/input-validation.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 while (true) {
   const number = Number(prompt("Enter a number between 50 and 100 (inclusive): "));
 
-  if (number <= 100) {
+  if (number <= 100 && number >= 50) {
     console.log("You entered a valid number. Exiting loop.");
     break;
   } else {
