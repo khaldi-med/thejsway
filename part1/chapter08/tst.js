46443af9ce60a9eec9262730cf26d9043adca67d
mohamed <khaldimohamed82@gmail.com>
2024-04-12 17:28:16 +0000

    Notes from my learning journey

---
+++ part1/chapter08/tst.js
@@ -0,0 +1,3 @@
+const monthList = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec";
+const months = monthList.split(",");
+console.log(months)
---
+++ part1/chapter08/word-info.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+const word = prompt("Enter a word: ");
+
+console.log(word.length)
+console.log(word.toLowerCase())
+console.log(word.toUpperCase())
