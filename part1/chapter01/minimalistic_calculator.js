303458299be554a7eb5c10523e5643f7f544c121
mohamed <khaldimohamed82@gmail.com>
2024-04-17 18:05:39 +0100

    Notes from my learning journey

---
+++ part1/chapter01/minimalistic_calculator.js
@@ -0,0 +1,11 @@
+// Adding
+console.log(`The result of 6 + 3 is: ${6 + 3}`);
+
+// Subtracting
+console.log(`The result of 6 - 3 is: ${6 - 3}`);
+
+// Multiplying
+console.log(`The result of 6 * 3 is: ${6 * 3}`);
+
+// Dividing
+console.log(`The result of 6 / 3 is: ${6 / 3}`);
