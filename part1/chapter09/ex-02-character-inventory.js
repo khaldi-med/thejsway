0106420a6e5c517c877ab66bd14260a5ca6d7ef5
mohamed <khaldimohamed82@gmail.com>
2024-04-19 23:58:07 +0100

    Notes from my learning journey

--- part1/chapter09/ex-02-character-inventory.js
+++ part1/chapter09/ex-02-character-inventory.js
@@ -5,14 +5,17 @@ class Character {
     this.name = name;
     this.health = health;
     this.strength = strength;
-    this.xp = 0;
+    this.xp = 0; // XP is always zero for new characters
+    this.gold = 10;    
+    this.key = 1;
   }
- 
+
+  // Attack a target
   attack(target) {
     if (this.health > 0) {
       const damage = this.strength;
       console.log(
-        `${this.name} attacks ${target.name} and causes ${damage} damage points`,
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`
       );
       target.health -= damage;
       if (target.health > 0) {
@@ -21,17 +24,20 @@ class Character {
         target.health = 0;
         const bonusXP = 10;
         console.log(
-          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`,
-        );
+          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${this.gold} gold and ${this.key} key(s)`);
         this.xp += bonusXP;
+        this.gold += target.gold;
+        this.key += target.key;
       }
     } else {
       console.log(`${this.name} can’t attack (they've been eliminated)`);
     }
   }
+  
   // Return the character description
   describe() {
-    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`;
+    return `${this.name} has ${this.health} health points, ${this
+      .strength} as strength and ${this.xp} XP points, ${this.gold} gold and ${this.key} key(s)`;
   }
 }
 
