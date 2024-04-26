59c895e401d8c0b937a5a7be783a0642a0dec8ee
mohamed <khaldimohamed82@gmail.com>
2024-04-26 02:07:49 +0100

    My learning journey practice

--- part1/part1_project/app.js
+++ part1/part1_project/app.js
@@ -11,29 +11,29 @@ while (true) {
   0 : Quit`
   );
   if (choosOp === option[0]) {
-    links.forEach((link, i) => {
-      alert(`${i + 1}: ${link}`);
-    });
-
+    alert(links);
   }
   else if (choosOp === option[1]) {
     const linkTitle = prompt("Enter the title: ");
     const linkUrl = prompt("Enter the url: ");
     const author = prompt("Enter the author: ");
 
-    let newLink = `${linkTitle} ${linkUrl}. Author: ${author}`;
+    let newLink = { link: `${linkTitle} ${linkUrl}. Author: ${author}`, index: 0 };
+    let newLinkIndex = `${newLink.index}: ${newLink.link}\n`;
 
     const httpProtocol = ["http://", "https://"]
     if (!(linkUrl.startsWith(httpProtocol[0]) || linkUrl.startsWith(httpProtocol[1]))) {
       const newLinkUrl = `https://${linkUrl}`;
-      newLink = `${linkTitle} ${newLinkUrl}. Author: ${author}`;
-      links.push(newLink);
+      newLinkIndex = `${newLink.index}: ${linkTitle} ${newLinkUrl}. Author: ${author}\n`;
+      links.push(newLinkIndex)
     }
     else {
-      links.push(newLink);
+      links.push(newLinkIndex)
     }
   }
   else if (choosOp === option[3]) {
     break;
   }
 }
+
+
