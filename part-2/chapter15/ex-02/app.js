95d0e88169932e4874df10e70eff9b9ed5d16f9a
mohamed <khaldimohamed82@gmail.com>
2024-05-19 19:15:05 +0100

    My learning journey practice

--- part-2/chapter15/ex-02/app.js
+++ part-2/chapter15/ex-02/app.js
@@ -1,14 +1,6 @@
 "use strict";
 
-const linkInfo = (link) => {
-      let allLinks = Array.from(document.querySelectorAll(link));
-      if (!allLinks.length) {
-              console.log("No links found");
-            } else {
-                    console.log(allLinks.length);
-                    console.log(allLinks[0].href);
-                    console.log(allLinks[allLinks.length - 1].href);
-                  }
-};
+const newspapers = ["https://www.nytimes.com", "https://www.washingtonpost.com", "http://www.economist.com"];
 
-linkInfo("a");
+const linkContent = newspapers.forEach(link =>
+  document.getElementById("content").insertAdjacentHTML('beforeend', `<a href="${link}">${link}</a></br>`))
