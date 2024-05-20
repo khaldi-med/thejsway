77276a4c218943a2dc279bae3845f46eb4e88c09
mohamed <khaldimohamed82@gmail.com>
2024-05-20 20:55:00 +0100

    My learning journey practice

---
+++ part-2/chapter15/ex-04/app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+"use strict";
+
+const words = [{
+    term: "Procrastination",
+    definition: "Avoidance of doing a task that needs to be accomplished",
+  },
+  {
+    term: "Tautology",
+    definition: "logical argument constructed in such a way that it is logically irrefutable",
+  },
+  {
+    term: "Oxymoron",
+    definition: "figure of speech that juxtaposes elements that appear to be contradictory",
+  },
+];
+
+const dictionaryList = document.createElement("dl");
+dictionaryList.id = "dictionary";
+document.getElementById("content").appendChild(dictionaryList);
+
+const dictionary = words.forEach((word) =>
+  document.getElementById("dictionary")
+  .insertAdjacentHTML("beforeend",
+    ` <dt><strong>${word.term}</strong></dt>
+                <dd> ${word.definition}</dd>
+                    `)
+);
