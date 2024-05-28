c5c09d40ea23a399af34d405dcba2cb9237c1888
mohamed <khaldimohamed82@gmail.com>
2024-05-28 18:05:59 +0100

    My learning journey practice

--- part-2/chapter15/ex-04/app.js
+++ part-2/chapter15/ex-04/app.js
@@ -1,15 +1,10 @@
 "use strict";
 
-
 const textColor = prompt("Enter the color for text: ")
- const backColor = prompt("Enter the background color for pragraph: ")
+const backColor = prompt("Enter the background color for pragraph: ")
 
-const textStyle = document.body 
-textStyle.style.color = textColor
-
-const setBackgorund = Array.from(document.getElementsByClassName('text'));
-setBackgorund.forEach((div) => {
+const textStyle = document.querySelectorAll("h1 + div");
+textStyle.forEach(div => {
+    div.style.color = textColor;
     div.style.backgroundColor = backColor;
-});
-
-console.log(setBackgorund);
+})
