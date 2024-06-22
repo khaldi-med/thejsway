41a5e01cce5b8da7c0ab61d10b7af3039e75f235
mohamed <khaldimohamed82@gmail.com>
2024-06-22 18:55:40 +0100

    My learning journey practice

--- part-2/chapter18/ex-01/app.js
+++ part-2/chapter18/ex-01/app.js
@@ -1,1 +1,18 @@
+"use strict";
 
+const controleButton = document.getElementById("action");
+const second = document.getElementById("count");
+let intervalId = null;
+
+controleButton.addEventListener("click", () => {
+	controleButton.textContent = "Stop";
+	if (intervalId === null) {
+		intervalId = setInterval(() => {
+			second.textContent = parseInt(second.textContent) + 1;
+		}, 1000);
+	} else {
+		clearInterval(intervalId);
+		intervalId = null;
+		controleButton.textContent = "Start";
+	}
+});
