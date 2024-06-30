56ea29742cab5f02fbfca0422f509a2088cd0ea7
mohamed <khaldimohamed82@gmail.com>
2024-06-30 17:05:28 +0100

    My learning journey practice

--- part-2/chapter18/ex-02/app.js
+++ part-2/chapter18/ex-02/app.js
@@ -1,25 +1,37 @@
-const start = document.getElementById("start");
-const movment = 5;
-const ball = document.getElementById("ball");
-const farm = document.getElementById("frame");
+"use strict";
+
+const ballElement = document.getElementById("ball");
+const fram = document.getElementById("frame");
+
+const widthBall = parseFloat(getComputedStyle(ball).width);
+const minX = 0;
+let direction = 1;
+const moveByPx = 7;
+let animationId = null;
 
 const moveBall = () => {
-	start.addEventListener("click", () => {
-		start.disabled = true;
-		stop.disabled = false;
+	const xBallStart = parseFloat(getComputedStyle(ball).left);
+	const maxFramWidth = parseFloat(getComputedStyle(frame).width);
 
-		const xBloc = parseFloat(getComputedStyle(ball).left);
-		ball.style.left = xBloc + movment + "px";
-		animationId = requestAnimationFrame(moveBall);
-	});
+	if (xBallStart + widthBall > maxFramWidth || xBallStart < minX) {
+		direction *= -1;
+	}
+	ballElement.style.left = `${xBallStart + direction * moveByPx}px`;
+	animationId = requestAnimationFrame(moveBall);
 };
-let animationId = requestAnimationFrame(moveBall);
 
-const stop = document.getElementById("stop");
-const stopBall = () => {
-	stop.addEventListener("click", () => {
-		stop.disabled = true;
-		start.disabled = false;
-		cancelAnimationFrame(animationId);
-	});
-};
+const startMove = document.getElementById("start");
+const stopMove = document.getElementById("stop");
+
+startMove.addEventListener("click", () => {
+	startMove.disabled = true;
+	stopMove.disabled = false;
+	animationId = requestAnimationFrame(moveBall);
+});
+
+stopMove.addEventListener("click", () => {
+	startMove.disabled = false;
+	stopMove.disabled = true;
+
+	cancelAnimationFrame(animationId);
+});
