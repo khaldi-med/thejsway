cc2a3b4850804bdf7dff9570d5f6c566042d74d6
mohamed <khaldimohamed82@gmail.com>
2024-07-01 23:22:29 +0100

    My learning journey practice

--- part-2/chapter19/app.js
+++ part-2/chapter19/app.js
@@ -1,5 +1,27 @@
 "use strict";
 
+class link {
+  constructor(title, url, author) {
+    let urlFormat = url;
+    if (!urlFormat.startWith("http://") && !urlFormat.startWith("https://")) {
+      urlFormat = `http://${urlFormat}`;
+    }
+    this.title = title;
+    this.url = url;
+    this.author = author;
+  }
+  toString() {
+    return `${this.title} ${this.url} Author: ${this.author}`;
+  }
+
+  createLinkElement() {
+    const linkElement = document.createElement("a");
+    linkElement.setAttribute("href", this.url);
+    linkElement.textContent = this.title;
+    return linkElement;
+  }
+}
+
 const button = document.getElementById("submitButton");
 const form = document.getElementById("content");
 
