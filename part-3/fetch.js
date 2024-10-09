c1851b34a5d87053fff8c43a989ee9091f96e09c
mohamed <khaldimohamed82@gmail.com>
2024-10-09 16:11:40 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ part-3/fetch.js
@@ -0,0 +1,9 @@
+'use strict'
+
+fetch(
+  "https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/languages.txt"
+)
+  .then(response => response.text()) // Access and return response's text content
+  .then(text => {
+    console.log(text); // Display file content in the console
+  });
