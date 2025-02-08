0794940e7a2a40dcdd14abd0e6124154fba7592a
mohamed <khaldimohamed82@gmail.com>
2025-02-08 21:56:44 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- part-3/chapter-22/github/github_api.js
+++ part-3/chapter-22/github/github_api.js
@@ -1,3 +1,14 @@
 'use strict'
 
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Bordeaux,fr&appid=YOUR_API_KEY&units=metric")
+.then(respones => respones.json())
+.then(content => console.log(content))
+let info = document.getElementById("info");
+info.textContent = "hello";
+
+.catch(err => {
+	console.error(err.message);
+});
+
+
 
--- part-3/fetch.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-fetch(
-  "https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/languages.txt"
-)
-  .then(response => response.text()) // Access and return response's text content
-  .then(text => {
-    console.log(text); // Display file content in the console
-  });
