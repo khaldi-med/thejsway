7a9f77caec543453705157b575f3f6aa7afd85de
mohamed <khaldimohamed82@gmail.com>
2025-02-12 22:33:25 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- part-3/chapter-22/github/github_api.js
+++ part-3/chapter-22/github/github_api.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const formElemnt = document.addEventListener("submit", e => {
+e.apperDefault();
+})
 fetch("https://api.github.com/users")
 .then(respones => respones.json())
 .then(user => console.log(user))
