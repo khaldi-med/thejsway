7aaedbb176a51dce2d18d7a8e7257f283b476ea2
mohamed <khaldimohamed82@gmail.com>
2025-07-07 16:29:57 +0100

    42 Cursus/ >> minishell update code <<

---
+++ part-3/chapter_24/circles.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.circumference = (radius) =>
+  `The circumference of a circl of ${radius} is ${2 * 3.14 * radius}`;
+module.exports.area = (radius) => `Its area is ${3.14 * radius * radius}`;
---
+++ part-3/chapter_24/index.js
@@ -0,0 +1,4 @@
+const radius = require("./circles.js");
+
+console.log(radius.circumference(5));
+console.log(radius.circumference(3));
